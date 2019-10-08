e639dbe705ce1fcb3bd2b41e884c2ed587fbe4db
zhujian <505169307@qq.com>
2019-10-08 16:22:28 +0800

    docs(dockerfile): 更新code安装和使用

--- dockerfiles/code/docker-entrypoint.sh
+++ dockerfiles/code/docker-entrypoint.sh
@@ -5,10 +5,11 @@ then
     USER_ID=${LOCAL_USER_ID:-9001}
  
     usermod -u ${USER_ID} -g ${USER_ID} -a -G root user
-    chown -R `id -u user`:`id -u user` /app
+    chown -R `id -u user`:`id -g user` /app
  
+    service dbus start
     export HOME=/home/user
-    exec gosu user "$0"
+    exec gosu user "$0" "$@"
 fi
 
-exec /usr/bin/code --verbose
+exec $@
